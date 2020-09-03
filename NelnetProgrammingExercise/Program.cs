79e8098202d46ab5ce6eabd62bfee875d2548cc3
Jacob Adkins <jakeadkins13@gmail.com>
2020-09-03 05:12:48 -0400

    Updated Program.cs
    removed items from program.cs and moved them to SetupWork.cs

--- NelnetProgrammingExercise/Program.cs
+++ NelnetProgrammingExercise/Program.cs
@@ -1,4 +1,5 @@
 ﻿using NelnetProgrammingExercise.Models;
+using NelnetProgrammingExercise.Work;
 using System;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -8,107 +9,107 @@ namespace NelnetProgrammingExercise
 {
     class Program
     {
-        private static PersonModel[] People;
-        private static PetModel[] Pets;
+        public static PersonModel[] People;
+        public static PetModel[] Pets;
 
         #region Initialization
 
-        private static void SetupObjects()
-        {
-            People = new PersonModel[]
-            {
-                new PersonModel()
-                {
-                    Name = "Dalinar",
-                    PreferredClassification = PetClassification.Mammal,
-                    PreferredPetType = PetType.Snake,
-                    PreferredPetSize = PetSize.Medium
-                },
-                new PersonModel()
-                {
-                    Name = "Kaladin",
-                    PreferredClassification = PetClassification.Bird,
-                    PreferredPetType = PetType.Goldfish,
-                    PreferredPetSize = PetSize.ExtraSmall
-                }
-            };
+        //private static void SetupObjects()
+        //{
+        //    People = new PersonModel[]
+        //    {
+        //        new PersonModel()
+        //        {
+        //            Name = "Dalinar",
+        //            PreferredClassification = PetClassification.Mammal,
+        //            PreferredPetType = PetType.Snake,
+        //            PreferredPetSize = PetSize.Medium
+        //        },
+        //        new PersonModel()
+        //        {
+        //            Name = "Kaladin",
+        //            PreferredClassification = PetClassification.Bird,
+        //            PreferredPetType = PetType.Goldfish,
+        //            PreferredPetSize = PetSize.ExtraSmall
+        //        }
+        //    };
 
-            Pets = new PetModel[]
-            {
-                new PetModel()
-                {
-                    Name = "Garfield",
-                    Weight = 20.00,
-                    Classification = PetClassification.Mammal,
-                    Type = PetType.Cat,
-                },
-                new PetModel()
-                {
-                    Name = "Odie",
-                    Weight = 15.00,
-                    Classification = PetClassification.Mammal,
-                    Type = PetType.Dog,
-                },
-                new PetModel()
-                {
-                    Name = "Peter Parker",
-                    Weight = 0.50,
-                    Classification = PetClassification.Arachnid,
-                    Type = PetType.Spider,
-                },
-                new PetModel()
-                {
-                    Name = "Kaa",
-                    Weight = 25.00,
-                    Classification = PetClassification.Reptile,
-                    Type = PetType.Snake,
-                },
-                new PetModel()
-                {
-                    Name = "Nemo",
-                    Weight = 0.50,
-                    Classification = PetClassification.Fish,
-                    Type = PetType.Goldfish,
-                },
-                new PetModel()
-                {
-                    Name = "Alpha",
-                    Weight = 0.10,
-                    Classification = PetClassification.Fish,
-                    Type = PetType.Betta,
-                },
-                new PetModel()
-                {
-                    Name = "Splinter",
-                    Weight = 0.50,
-                    Classification = PetClassification.Mammal,
-                    Type = PetType.Rat,
-                },
-                new PetModel()
-                {
-                    Name = "Coco",
-                    Weight = 06.00,
-                    Classification = PetClassification.Bird,
-                    Type = PetType.Parrot,
-                },
-                new PetModel()
-                {
-                    Name = "Tweety",
-                    Weight = 0.05,
-                    Classification = PetClassification.Bird,
-                    Type = PetType.Canary,
-                }
-            };
+        //    Pets = new PetModel[]
+        //    {
+        //        new PetModel()
+        //        {
+        //            Name = "Garfield",
+        //            Weight = 20.00,
+        //            Classification = PetClassification.Mammal,
+        //            Type = PetType.Cat,
+        //        },
+        //        new PetModel()
+        //        {
+        //            Name = "Odie",
+        //            Weight = 15.00,
+        //            Classification = PetClassification.Mammal,
+        //            Type = PetType.Dog,
+        //        },
+        //        new PetModel()
+        //        {
+        //            Name = "Peter Parker",
+        //            Weight = 0.50,
+        //            Classification = PetClassification.Arachnid,
+        //            Type = PetType.Spider,
+        //        },
+        //        new PetModel()
+        //        {
+        //            Name = "Kaa",
+        //            Weight = 25.00,
+        //            Classification = PetClassification.Reptile,
+        //            Type = PetType.Snake,
+        //        },
+        //        new PetModel()
+        //        {
+        //            Name = "Nemo",
+        //            Weight = 0.50,
+        //            Classification = PetClassification.Fish,
+        //            Type = PetType.Goldfish,
+        //        },
+        //        new PetModel()
+        //        {
+        //            Name = "Alpha",
+        //            Weight = 0.10,
+        //            Classification = PetClassification.Fish,
+        //            Type = PetType.Betta,
+        //        },
+        //        new PetModel()
+        //        {
+        //            Name = "Splinter",
+        //            Weight = 0.50,
+        //            Classification = PetClassification.Mammal,
+        //            Type = PetType.Rat,
+        //        },
+        //        new PetModel()
+        //        {
+        //            Name = "Coco",
+        //            Weight = 06.00,
+        //            Classification = PetClassification.Bird,
+        //            Type = PetType.Parrot,
+        //        },
+        //        new PetModel()
+        //        {
+        //            Name = "Tweety",
+        //            Weight = 0.05,
+        //            Classification = PetClassification.Bird,
+        //            Type = PetType.Canary,
+        //        }
+        //    };
             
-        }
-        private static void SetupPets()
-        {
-            foreach (PetModel pet in Pets)
-            {
-                pet.Size = WeightWork.weightWork(pet.Weight);
+        //}
+        //private static void SetupPets()
+        //{
+        //    foreach (PetModel pet in Pets)
+        //    {
+        //        pet.Size = WeightWork.weightWork(pet.Weight);
                
-            }
-        }
+        //    }
+        //}
         #endregion
 
         private static string IsGood(PersonModel person, PetModel pet)
@@ -120,7 +121,7 @@ namespace NelnetProgrammingExercise
 
         static void Main(string[] args)
         {
-            SetupObjects();
+            SetupWork.SetupObjects();
 
             foreach(PersonModel person in People) {
                 Console.WriteLine(string.Format("Pets for {0}:", person.Name));
