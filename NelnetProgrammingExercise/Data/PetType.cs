57ef72e5ba83fb9c23fe457da95fb99d4ca5c064
Jacob Adkins <jakeadkins13@gmail.com>
2020-09-03 04:17:49 -0400

    Req 1 - Added weight to each pet
    Added weight to each pet and updated the pet model

---
+++ NelnetProgrammingExercise/Data/PetType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace NelnetProgrammingExercise.Models
+{
+    public enum PetType
+    {
+        Cat,
+        Dog,
+        Spider,
+        Snake,
+        Goldfish,
+        Betta,
+        Rat,
+        Parrot,
+        Canary
+    }
+}
--- NelnetProgrammingExercise/Models/PetModel.cs
+++ NelnetProgrammingExercise/Models/PetModel.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Transactions;
 
 namespace NelnetProgrammingExercise.Models
 {
+
     public class PetModel
     {
         public string Name { get; set; }
+        public double Weight { get; set; }
         public PetClassification Classification { get; set; }
         public PetType Type { get; set; }
+        public PetSize Size { get; set; }
+     
     }
 }
